84eacc178ff1d31378d9864a768e9ebd3eb0be8a
shakiz <activities335@gmail.com>
2019-08-21 15:32:50 +0600

    Updated new room layout and navigate it with main activity.

--- app/src/main/java/com/shakil/homeapp/activities/MainActivity.java
+++ app/src/main/java/com/shakil/homeapp/activities/MainActivity.java
@@ -2,15 +2,35 @@ package com.shakil.homeapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.shakil.homeapp.R;
+import com.shakil.homeapp.activities.room.AddNewRoomActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView addRoom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        addRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddNewRoomActivity.class));
+            }
+        });
+    }
+
+    private void init() {
+        addRoom = findViewById(R.id.mAddMasterRoom);
     }
 }
