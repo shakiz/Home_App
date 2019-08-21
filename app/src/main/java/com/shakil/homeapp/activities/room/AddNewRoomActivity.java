c3f1a245e92a570e4474256016d483ddc7d7535e
shakiz <activities335@gmail.com>
2019-08-21 15:06:41 +0600

    Added new room and meter activity.

--- app/src/main/java/com/shakil/homeapp/activities/room/AddNewRoomActivity.java
+++ app/src/main/java/com/shakil/homeapp/activities/room/AddNewRoomActivity.java
@@ -1,14 +1,16 @@
-package com.shakil.homeapp;
+package com.shakil.homeapp.activities.room;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.shakil.homeapp.R;
+
+public class AddNewRoomActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_new_room);
     }
 }
