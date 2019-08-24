8ffdbcf72c78d24f604a76221b87cb12491a2e83
shakiz <activities335@gmail.com>
2019-08-24 20:03:51 +0600

    Added the spinner adapter and spinner data class.

---
+++ app/src/main/java/com/shakil/homeapp/activities/utils/SpinnerData.java
@@ -0,0 +1,10 @@
+package com.shakil.homeapp.activities.utils;
+
+
+public class SpinnerData {
+
+    public String[] setMonthData(){
+        String[] spinnerValues = {"January","February","March","April","May","June","July","August","September","October","November","December"};
+        return spinnerValues;
+    }
+}
