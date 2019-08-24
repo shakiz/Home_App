c1348f811e22e383c4f5d72e263399b993c18b0c
shakiz <activities335@gmail.com>
2019-08-24 20:02:29 +0600

    Updated the add room layout and added some functionality.

--- app/src/main/java/com/shakil/homeapp/activities/room/AddNewRoomActivity.java
+++ app/src/main/java/com/shakil/homeapp/activities/room/AddNewRoomActivity.java
@@ -3,14 +3,60 @@ package com.shakil.homeapp.activities.room;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.shakil.homeapp.R;
+import com.shakil.homeapp.activities.utils.SpinnerAdapter;
+import com.shakil.homeapp.activities.utils.SpinnerData;
 
 public class AddNewRoomActivity extends AppCompatActivity {
+
+    private Spinner monthSpinner;
+    private FloatingActionButton addRoomButton;
+    private EditText roomName,tenantName,advanceAmount;
+    private CheckBox checkBoxAdvance;
+    private LinearLayout linearLayoutAdvanceAmount;
+    private SpinnerData spinnerData;
+    private SpinnerAdapter spinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_room);
+
+        init();
+
+        spinnerAdapter.setSpinnerAdapter(monthSpinner,spinnerData.setMonthData(),this);
+
+        checkBoxAdvance.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean visibilityValue) {
+                if (visibilityValue){
+                    linearLayoutAdvanceAmount.setVisibility(View.VISIBLE);
+                }
+                else{
+                    linearLayoutAdvanceAmount.setVisibility(View.GONE);
+                }
+            }
+        });
     }
+
+    private void init() {
+        monthSpinner = findViewById(R.id.MonthSpinner);
+        addRoomButton = findViewById(R.id.mSaveRoomMaster);
+        roomName = findViewById(R.id.RoomName);
+        tenantName = findViewById(R.id.TenantName);
+        advanceAmount = findViewById(R.id.AdvanceAmount);
+        checkBoxAdvance = findViewById(R.id.AdvanceCehckBox);
+        linearLayoutAdvanceAmount = findViewById(R.id.advanceAmountLayout);
+        spinnerData = new SpinnerData();
+        spinnerAdapter = new SpinnerAdapter();
+    }
+
+
 }
